d768b4060f77052e330c6904f8a8ac709464b6ef
Clems-web <clement.servais1@gmail.com>
2021-01-18 13:42:14 +0100

    finito

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
     $texte = "Du texte est stocké";
-    echo $texte;
+    echo $texte . "<br>";
     $y = 30;
-    echo "y vaut $y";
+    echo "y vaut $y" . "<br>";
     $m = 5;
     $n = 7;
     echo $m + $n;
